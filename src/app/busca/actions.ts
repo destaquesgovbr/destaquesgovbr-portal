7420156905e53fde03dc1fc72ac4c199939cc183
Pedro Vinagre <vinagrebraga@gmail.com>
2025-10-23 18:39:22 -0300

    Add query actions

---
+++ src/app/busca/actions.ts
@@ -0,0 +1,67 @@
+'use server'
+
+import type { ArticleRow } from '@/lib/article-row'
+import { typesense } from '@/lib/typesense-client'
+
+export type GetArticlesArgs = {
+  query?: string
+  cursor?: string
+}
+
+export type GetArticlesResult = {
+  articles: ArticleRow[]
+  cursor: string | null
+}
+
+type CursorPayload = {
+  publishedAt: number
+  query?: string
+  id: string
+}
+
+const PAGE_SIZE = 40
+
+function decodeCursor(cursor: string): CursorPayload {
+  const decoded = Buffer.from(cursor, 'base64url').toString('utf-8')
+  const [publishedAt, id] = decoded.split(':')
+  return { publishedAt: parseInt(publishedAt), id }
+}
+
+function encodeCursor(publishedAt: number, id: string): string {
+  const toEncode = `${publishedAt}:${id}`
+  return Buffer.from(toEncode).toString('base64url')
+}
+
+export async function getArticles(
+  args: GetArticlesArgs,
+): Promise<GetArticlesResult> {
+  const cursor = args.cursor ? decodeCursor(args.cursor) : null
+
+  let filter_by = ''
+
+  if (cursor) {
+    filter_by = `published_at:<=${cursor.publishedAt} || (published_at:=${cursor.publishedAt} && unique_id:!=${cursor.id})`
+  }
+
+  // biome-ignore format: true
+  const result = await typesense
+    .collections<ArticleRow>('news')
+    .documents()
+    .search({
+      q: args.query ?? '*',
+      query_by: 'title, content',
+      sort_by: 'published_at:desc, unique_id:desc',
+      filter_by,
+      limit: PAGE_SIZE
+    })
+
+  const lastResult = result.hits?.at(-1)?.document
+
+  return {
+    articles: result.hits?.map(hit => hit.document) ?? [],
+    cursor:
+      result.hits?.length === PAGE_SIZE
+        ? encodeCursor(lastResult!.published_at!, lastResult!.unique_id)
+        : null,
+  }
+}
