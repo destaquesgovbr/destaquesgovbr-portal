f223708ea8c392a5e398e68a13154b11d395efae
Pedro Vinagre <vinagrebraga@gmail.com>
2025-10-27 20:13:44 -0300

    Add basic date filters

--- src/app/artigos/page.tsx
+++ src/app/artigos/page.tsx
@@ -4,17 +4,27 @@ import { useInfiniteQuery } from '@tanstack/react-query'
 import { useInView } from 'react-intersection-observer'
 import NewsCard from '@/components/NewsCard'
 import { getArticles } from './actions'
+import { Input } from '@/components/ui/input'
+import { useEffect, useState } from 'react'
+import { X } from 'lucide-react'
 
 export default function ArticlesPage() {
-  function queryFn({ pageParam }: { pageParam: string | null }) {
-    return getArticles({ cursor: pageParam ?? undefined })
+  const [startDate, setStartDate] = useState<Date>()
+  const [endDate, setEndDate] = useState<Date>()
+
+  function queryFn({ pageParam }: { pageParam: number | null }) {
+    return getArticles({
+      page: pageParam ?? 1,
+      startDate: startDate?.getTime(),
+      endDate: endDate?.getTime()
+    })
   }
 
   const articlesQ = useInfiniteQuery({
     queryKey: ['articles'],
     queryFn,
-    getNextPageParam: (lastPage) => lastPage.cursor ?? undefined,
-    initialPageParam: null,
+    getNextPageParam: (lastPage) => lastPage.page ?? undefined,
+    initialPageParam: 1,
   })
 
   const { ref } = useInView({
@@ -26,6 +36,10 @@ export default function ArticlesPage() {
   })
 
   const articles = articlesQ.data?.pages.flatMap((page) => page.articles) ?? []
+
+  useEffect(() => {
+    articlesQ.refetch()
+  }, [startDate, endDate])
 
   if (articlesQ.isError) {
     return (
@@ -42,6 +56,44 @@ export default function ArticlesPage() {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between mb-8">
           <h2 className="text-2xl font-bold">Últimas notícias</h2>
+
+          <div className="flex gap-4">
+            <div>
+              <span>A partir de:</span>
+              <div className="relative w-full">
+                <Input
+                  type="date"
+                  onChange={(e) => setStartDate(new Date(e.target.value))}
+                  className={startDate ? "pr-9" : undefined}
+                  value={startDate ? startDate.toISOString().split("T")[0] : ""}
+                />
+                {startDate &&
+                  <X
+                    onClick={() => setStartDate(undefined)}
+                    className="absolute right-1 top-1/2 h-8 w-8 -translate-y-1/2 text-muted-foreground rounded-full p-2 hover:bg-gray-200 hover:cursor-pointer"
+                  />
+                }
+              </div>
+            </div>
+
+            <div>
+              <span>Publicado até:</span>
+              <div className="relative w-full">
+                <Input
+                  type="date"
+                  onChange={(e) => setEndDate(new Date(e.target.value))}
+                  className={endDate ? "pr-9" : undefined}
+                  value={endDate ? endDate.toISOString().split("T")[0] : ""}
+                />
+                {endDate &&
+                  <X
+                    onClick={() => setEndDate(undefined)}
+                    className="absolute right-1 top-1/2 h-8 w-8 -translate-y-1/2 text-muted-foreground rounded-full p-2 hover:bg-gray-200 hover:cursor-pointer"
+                  />
+                }
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
