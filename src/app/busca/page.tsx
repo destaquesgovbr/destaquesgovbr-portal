222bb349428343d26b59f22423bc02c21e10c2e0
Pedro Vinagre <vinagrebraga@gmail.com>
2025-10-27 20:20:22 -0300

    Add date filters

--- src/app/busca/page.tsx
+++ src/app/busca/page.tsx
@@ -6,6 +6,8 @@ import { useInView } from 'react-intersection-observer'
 import NewsCard from '@/components/NewsCard'
 import { queryArticles } from './actions'
 import { useEffect, useState } from 'react'
+import { Input } from '@/components/ui/input'
+import { X } from 'lucide-react'
 
 export default function QueryPage() {
   const searchParams = useSearchParams()
@@ -59,6 +61,44 @@ export default function QueryPage() {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between mb-8">
           <h2 className="text-2xl font-bold">Resultados para "{query}"</h2>
+
+          <div className="flex gap-4">
+            <div>
+              <span>A partir de:</span>
+              <div className="relative w-full">
+                <Input
+                  type="date"
+                  onChange={(e) => setStartDate(new Date(e.target.value))}
+                  className={startDate ? "pr-9" : undefined}
+                  value={startDate ? startDate.toISOString().split("T")[0] : ""}
+                />
+                {startDate &&
+                  <X
+                    onClick={() => setStartDate(undefined)}
+                    className="absolute right-1 top-1/2 h-8 w-8 -translate-y-1/2 text-muted-foreground rounded-full p-2 hover:bg-gray-200 hover:cursor-pointer"
+                  />
+                }
+              </div>
+            </div>
+
+            <div>
+              <span>Publicado até:</span>
+              <div className="relative w-full">
+                <Input
+                  type="date"
+                  onChange={(e) => setEndDate(new Date(e.target.value))}
+                  className={endDate ? "pr-9" : undefined}
+                  value={endDate ? endDate.toISOString().split("T")[0] : ""}
+                />
+                {endDate &&
+                  <X
+                    onClick={() => setEndDate(undefined)}
+                    className="absolute right-1 top-1/2 h-8 w-8 -translate-y-1/2 text-muted-foreground rounded-full p-2 hover:bg-gray-200 hover:cursor-pointer"
+                  />
+                }
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
