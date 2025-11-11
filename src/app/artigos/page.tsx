a245e8c7e86d5e84b82617078e0b770868434527
Pedro Vinagre <vinagrebraga@gmail.com>
2025-11-11 01:57:37 -0300

    Update page

--- src/app/artigos/page.tsx
+++ src/app/artigos/page.tsx
@@ -1,8 +1,10 @@
 import { getAgenciesList } from '@/lib/agencies-utils'
+import { getThemesList } from '@/lib/themes-utils'
 import ArticlesPageClient from './ArticlesPageClient'
 
 export default async function ArticlesPage() {
   const agencies = await getAgenciesList()
+  const themes = await getThemesList()
 
-  return <ArticlesPageClient agencies={agencies} />
+  return <ArticlesPageClient agencies={agencies} themes={themes} />
 }
