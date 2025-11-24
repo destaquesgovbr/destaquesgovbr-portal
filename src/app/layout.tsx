9b5c99b5c884a71a423c78dff958a85ff3d84df9
Pedro Vinagre <vinagrebraga@gmail.com>
2025-11-24 14:58:49 -0300

    Update header padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       >
         <Providers>
           <Header />
-          <div className="pt-[140px] md:pt-[180px]">
+          <div className="pt-[110px] md:pt-[130px]">
             <Suspense>
               {children}
             </Suspense>
